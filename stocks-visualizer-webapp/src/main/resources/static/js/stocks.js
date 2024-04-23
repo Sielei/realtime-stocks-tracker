783b9e7a7149c0d57765b32cdbc6842a4d738acc
Sielei Herman <hsielei@gmail.com>
2024-04-23 17:37:32 +0300

    feat: Use CanvasJS dynamic chart to display realtime stock price.

---
+++ stocks-visualizer-webapp/src/main/resources/static/js/stocks.js
@@ -0,0 +1,59 @@
+window.onload = function () {
+    let dps = [];
+    let chart = new CanvasJS.Chart('container', {
+        title: {
+            text: 'Realtime Stock Prices'
+        },
+        data: [
+            {
+                type: 'line',
+                dataPoints: dps
+            }
+        ]
+    });
+
+    let xVal = new Date();
+    let yVal = 0;
+    let updateInterval = 1000;
+    let dataLength = 20;
+    
+    let updateChart = function (count) {
+        count = count || 1;
+
+        const eventSource = new EventSource("http://localhost:8182/api/v1/stocks");
+        eventSource.addEventListener('stockQuote', (event) => {
+            const symbol = JSON.parse(event.data).symbol;
+            const time = JSON.parse(event.data).tradeTime;
+            const tradeValue = JSON.parse(event.data).tradeValue;
+            if (symbol === 'AAPL'){
+                xVal = new Date(time);
+                yVal = tradeValue;
+                if (!checkIfDPExists(dps, xVal)){
+                    dps.push({x:xVal, y: yVal});
+                }
+            }
+        });
+        if (dps.length > dataLength) {
+            dps.shift();
+        }
+
+        chart.render();
+    }
+    updateChart(dataLength);
+    setInterval(function(){updateChart()}, updateInterval);
+}
+
+let checkIfDPExists = (dps, currDate) =>{
+    if (!dps.length){
+        return false;
+    }
+    for (let i = 0; i < dps.length; i++){
+        if ((dps[i].x).toISOString() === currDate.toISOString()){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
